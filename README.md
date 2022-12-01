09d8e647af207b057d99e0d8677b5e623a54389d
Frank Terpstra <frank@frankterpstra.com>
2022-12-01 09:01:35 +0100

    respec configuratie

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SLD
-Repository voor de ontwikkeling van BRO fase 2 registratie object SLD
+Repository voor de ontwikkeling van BRO fase 2 registratie object Overheidsbesluit bodemverontreiniging (SLD)
+
+zie https://broprogramma.github.io/SLD voor de werkversie
+
+
---
+++ config.js
@@ -0,0 +1,41 @@
+var respecConfig = {
+  specStatus: "GN-WV",
+  specType: "IM",
+  //subtitle: "SIKB en Geonovum informatiemodel",
+  //canonicalURI: "WV",
+  //publishDate: "2021-11-19",
+  maxTocLevel: 4,
+ logos: [{
+    src: 'https://docs.geostandaarden.nl/bro/gen/logo_minbzk.png',
+    href: "https://www.geobasisregistraties.nl/basisregistraties/ondergrond",
+    alt: "Ministerie BZK",
+    width: 776,
+    height: 163,
+    id: 'logo_minbzk',
+  }]  ,	
+  editors: [
+    {
+      name: "SIKB",
+      url: "https://www.sikb.nl",
+	},
+	{
+      name: "Geonovum",
+      url: "https://www.geonovum.nl",	  
+    },  
+  ],
+  authors: [
+    {
+      name: "SIKB",
+      url: "https://www.sikb.nl",
+	},
+	{
+      name: "Geonovum",
+	  url: "https://www.geonovum.nl",
+	},
+	],
+  shortName: "sld",
+  pubDomain: "bro",
+  github: "https://github.com/BROprogramma/SLD",
+//  previousPublishDate: "2021-11-19",
+//  previousStatus: "GN-VV",
+};
